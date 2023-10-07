3aa49612cf1d4a982241d95cda3ffa257aa43b34
Sarapuce <alexandre.herve97@gmail.com>
2023-10-07 20:04:05 +0200

    feat(app): css

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+head = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/static/css/style.css">
+    <title>IWard</title>
+</head>
+"""
+
 @app.route("/", methods=["GET"])
 def main_route():
-    return "Welcome to this application, check /vulnerable?name=alex or /not_vulnerable?name=alex"
+    return head + "Welcome to this application, check /vulnerable?name=alex or /not_vulnerable?name=alex"
 
 
 @app.route("/vulnerable", methods=["GET"])
@@ -12,7 +22,7 @@ def vulnerable():
     name = request.args.get('name')
     if not name:
         return "You must give me your name if you want me to welcome you"
-    return "Hello {}, how are you ?".format(name)
+    return head + "Hello {}, how are you ?".format(name)
 
 @app.route("/not_vulnerable", methods=["GET"])
 def not_vulnerable():
