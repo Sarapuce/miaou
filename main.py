114ac61a1aa10ce2c9659ae4c6ce30dd72b741bc
Sarapuce <alexandreh@padok.fr>
2023-12-26 10:56:36 +0100

    feat(templates): add interactive name

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ head = """
     <meta charset="UTF-8">
     <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
     <link rel="stylesheet" href="/static/css/style.css">
-    <title>IWard</title>
+    <title>Miaou</title>
 </head>
 """
 
@@ -17,19 +17,49 @@ cat1 = """
 </br>
 </br>
 </br>
-<img src=/static/img/img1.jpg>
+<img src=/static/img/img1.jpg></br>
+<p>&nbsp;&nbsp;／l、&nbsp;&nbsp;&nbsp;&nbsp;    I sleep...</br>     
+（ﾟ､ ｡ ７</br>         
+&nbsp;&nbsp;l&nbsp;&nbsp;~ヽ</br>       
+&nbsp;&nbsp;じしf_,)ノ</br></p>
 """
 
 cat2 = """
 </br>
 </br>
 </br>
-<img src=/static/img/img2.jpg>
+<img src=/static/img/img3.jpg></br>
+<p>&nbsp;&nbsp;／l、&nbsp;&nbsp;&nbsp;&nbsp;    I's dangerous</br>     
+（ﾟ､ ｡ ７</br>         
+&nbsp;&nbsp;l&nbsp;&nbsp;~ヽ</br>       
+&nbsp;&nbsp;じしf_,)ノ</br></p>
+"""
+
+index = """
+    <form id="nameForm">
+        <label for="username">What's your name?</label>
+        <input type="text" id="username" name="username" required>
+        <br>
+        <button type="button" onclick="redirectToHelloSafe()">Say Hello in a safe way</button>
+        <button type="button" onclick="redirectToHelloDangerous()">Say Hello in a dangerous way</button>
+    </form>
+
+    <script>
+    function redirectToHelloSafe() {
+        var username = encodeURIComponent(document.getElementById('username').value);
+        window.location.href = '/not_vulnerable?name=' + username;
+    }
+
+    function redirectToHelloDangerous() {
+        var username = encodeURIComponent(document.getElementById('username').value);
+        window.location.href = '/vulnerable?name=' + username;
+    }
+    </script>
 """
 
 @app.route("/", methods=["GET"])
 def main_route():
-    return head + "Welcome to this application, check /vulnerable?name=alex or /not_vulnerable?name=alex" + cat1
+    return head + index + "<br>Welcome to this application, check /vulnerable?name=alex or /not_vulnerable?name=alex" + cat1
 
 
 @app.route("/vulnerable", methods=["GET"])
